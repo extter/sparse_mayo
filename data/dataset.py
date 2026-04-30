78e2eb1599e7eb8233ab4efd7284f2ab2e6c413d
Catalin <catasajin@gmail.com>
2026-04-30 12:50:39 +0200

    added image opening script

--- data/dataset.py
+++ data/dataset.py
@@ -1,1 +1,24 @@
 # Dataset class and DataLoader setup
+from torch.utils.data import Dataset
+from PIL import Image
+import os
+
+class MayoDataset(Dataset):
+
+    def __init__(self, image_paths, transform=None):
+        self.image_paths = image_paths
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+
+        img_path = self.image_paths[idx]
+
+        image = Image.open(img_path).convert("L")
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image
--- data/preprocessing.py
+++ data/preprocessing.py
@@ -1,1 +1,10 @@
-# Normalization, resizing to 256x256
+# Normalization, resizing to 256x256, transforming in tensor
+from torchvision import transforms
+
+def get_transform(image_size=256):
+    transform = transforms.Compose([
+        transforms.Resize((image_size, image_size)), #interpolation: BILINEAR
+        transforms.ToTensor() #returns a tensor like [channel, H, W] with values [0.,1.]
+        ])
+
+    return transform
