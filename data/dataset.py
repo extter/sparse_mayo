0d99cf152cfd51e56e952f164462b77a775bcf3f
Catalin <catasajin@gmail.com>
2026-05-05 16:05:03 +0200

    finished first notebook. now changing the idea behind the second

--- data/dataset.py
+++ data/dataset.py
@@ -1,10 +1,11 @@
 # Dataset class and DataLoader setup
 from torch.utils.data import Dataset
 from PIL import Image
-import os
+import torch
+import numpy as np
 
 class MayoDataset(Dataset):
-
+    """Carica immagini raw (.png) e applica transform"""
     def __init__(self, image_paths, transform=None):
         self.image_paths = image_paths
         self.transform = transform
@@ -13,12 +14,20 @@ class MayoDataset(Dataset):
         return len(self.image_paths)
 
     def __getitem__(self, idx):
-
-        img_path = self.image_paths[idx]
-
-        image = Image.open(img_path).convert("L")
-
+        image = Image.open(self.image_paths[idx]).convert("L")
         if self.transform:
             image = self.transform(image)
-
         return image
+
+
+class MayoDatasetNpy(Dataset):
+    """Carica immagini già preprocessate (.npy), float32 [0,1]"""
+    def __init__(self, image_paths):
+        self.image_paths = image_paths
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        image_np = np.load(self.image_paths[idx])          # [256, 256]
+        return torch.from_numpy(image_np).unsqueeze(0)     # [1, 256, 256]
